8aab4c992fc8b4edca18faa39383c547e97249e6
Sumit Kandwal <82891876+kandwal811@users.noreply.github.com>
2023-07-13 21:52:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ import { Server } from "socket.io";
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatly-sk.onrender.com",
   },
 });
 
